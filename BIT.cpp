6e2ecd179c4d4694fbd8b7d96e163fecceb2e551
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-04-27 13:26:06 +0530

    Binary Indexed Tree for Updating the values in an array
    Binary Indexed Tree for Updating the values in an array

---
+++ BIT.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<iostream>
+
+int n; // length of the array
+int a[10001]; //array
+int BIT[10001]; 
+int x; // lower limit of query range
+int y; // upper limit of query range
+
+int v; //the value that needs to be updated
+
+// function to query using BIT
+int query(int k) {
+	int sum = 0;
+	while (k > 0) {
+		sum += BIT[k];
+		k -= k & -k;
+	}
+	return sum;
+}
+
+
+// function to update BIT
+void update(int index, int value) {
+	while (index <=n)
+	{
+		BIT[index] += value;
+		index += index & -index;
+	}
+}
+int main() {
+
+	printf("Enter the length of the array ");
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+		// updating the Binary Indexed tree
+		update(i, a[i]);
+		update(i + 1, -a[i]);
+	}
+	printf("Enter the value you want to add and range for updating values: \n");
+	scanf("%d %d %d", &v, &x, &y);
+
+	// no need to run a loop in this case
+	update(x, v);
+	update(y + 1, -v);
+	for (int i = x; i <= y; i++)
+		printf("updated %dth element is %d\n", i, query(i));
+	getchar();
+	return 0;
+
+}
